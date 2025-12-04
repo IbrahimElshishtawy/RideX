7f6e538df8410201ae099e4cb9788f00178fc556
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-04 15:04:09 +0200

    Create app_router.dart

---
+++ ridex/lib/core/router/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:go_router/go_router.dart';
+import 'package:ridex/features/auth/presentation/screens/splash_welcome_screen.dart';
+import 'package:ridex/features/auth/presentation/screens/onboarding_carousel_screen.dart';
+
+class AppRouter {
+  GoRouter get router => _router;
+
+  final GoRouter _router = GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const SplashWelcomeScreen(),
+      ),
+      GoRoute(
+        path: '/onboarding',
+        builder: (context, state) => const OnboardingCarouselScreen(),
+      ),
+    ],
+  );
+}
