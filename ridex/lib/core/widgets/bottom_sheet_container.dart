84e5d6f3eed9607c2e46295b0ea14063006d45fc
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-04 15:03:48 +0200

    Create bottom_sheet_container.dart

---
+++ ridex/lib/core/widgets/bottom_sheet_container.dart
@@ -0,0 +1,35 @@
+﻿import 'package:flutter/material.dart';
+
+class BottomSheetContainer extends StatelessWidget {
+  final Widget child;
+  final double radius;
+  final EdgeInsets padding;
+
+  const BottomSheetContainer({
+    super.key,
+    required this.child,
+    this.radius = 22,
+    this.padding = const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.colorScheme.surface,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(radius)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.12),
+            blurRadius: 12,
+            offset: Offset(0, -3),
+          ),
+        ],
+      ),
+      padding: padding,
+      child: child,
+    );
+  }
+}
