9f27aba78eaae5308cc802a2a0295aebf8596c0f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-04 15:03:24 +0200

    Create section_title.dart

---
+++ ridex/lib/core/widgets/section_title.dart
@@ -0,0 +1,18 @@
+﻿import 'package:flutter/material.dart';
+
+class SectionTitle extends StatelessWidget {
+  final String text;
+
+  const SectionTitle({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        fontSize: 19,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+  }
+}
