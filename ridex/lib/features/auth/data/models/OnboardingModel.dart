0b2a29bebba089d312db69f2952ef84280999581
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-06 15:40:02 +0200

    Create OnboardingModel.dart

---
+++ ridex/lib/features/auth/data/models/OnboardingModel.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: file_names
+
+class OnboardingModel {
+  final String title;
+  final String desc;
+  final String image;
+
+  const OnboardingModel({
+    required this.title,
+    required this.desc,
+    required this.image,
+  });
+}
